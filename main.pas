d615e03346ce3ad9aa03bf88b688e09a0f055724
alexandersumaneev <alexander_sumaneev@hotmail.com>
2017-04-09 13:43:51 +0300

    cmt

--- main.pas
+++ main.pas
@@ -3,14 +3,14 @@
  * User: alexa
  * Date: 06.04.2017
  *)
-program FracSierp2;
+program fractal;
 uses wincrt, graph;
 
 var
-    gd, gm,VisualPage : Integer;
+    gd, gm,visualpage : Integer;
     iter:integer;
+    kp:Boolean;
     x1,x2,x3,y1,y2,y3,dx2,dx3,dy2,dy3:Real;
-    sauser:Pointer;
 procedure tr(x1, y1, x2, y2, x3, y3: Real);
 begin
     Line(Round(x1), Round(y1), Round(x2), Round(y2));
@@ -31,7 +31,6 @@ begin
         x3n := (x3 + x1) / 2;
         y3n := (y3 + y1) / 2;
         tr(x1n, y1n, x2n, y2n, x3n, y3n);
-
         draw(x1, y1, x1n, y1n, x3n, y3n, n - 1);
         draw(x2, y2, x1n, y1n, x2n, y2n, n - 1);
         draw(x3, y3, x2n, y2n, x3n, y3n, n - 1);
@@ -40,80 +39,61 @@ end;
 
 procedure key_pres();
 var
-key:char;
+    key:char;
 begin
-    SetVisualPage(1- (VisualPage mod 2));
-{установка номера видимой видеостраницы}
-    VisualPage := VisualPage+1; {листание видеостраниц}
-    SetActivePage(1 - (VisualPage mod 2));
-{установка номера невидимой (активной) видеостраницы,}
-{используемой для построения смещенного изображения }
-    ClearDevice; {очистка графического экрана}
+    setvisualpage(1- (visualpage mod 2));
+    visualpage := visualpage+1;
+    setactivepage(1 - (visualpage mod 2));
+    cleardevice;
+    x2:=x1+dx2;x3:=x1+dx3;
+    y2:=y1+dy2;y3:=y1+dy3;
     tr(x1,y1,x2,y2,x3,y3);
     draw(x1,y1,x2,y2,x3,y3,iter);
-key:=readkey();
+    key:=readkey();
     Case key Of
-    #27: Halt();
-    'a':
+    #27:
     begin
-        x1:=x1-20;
-        x2:=x2-20;
-        x3:=x3-20;
+        kp:=false;
+        closegraph();
     end;
-    'd':
-    begin
-        x1:=x1+20;
-        x2:=x2+20;
-        x3:=x3+20;
-    end;
-    's':
-    begin
-        y1:=y1+20;
-        y2:=y2+20;
-        y3:=y3+20;
-    end;
-    'w':
-    begin
-        y1:=y1-20;
-        y2:=y2-20;
-        y3:=y3-20;
-    end;
+    'a': x1:=x1-20;
+    'd': x1:=x1+20;
+    's': y1:=y1+20;
+    'w': y1:=y1-20;
     'z':
     begin
-        y1:=y1/1.1;
-        y2:=y2/1.1;
-        y3:=y3/1.1;
-        x1:=x1/1.1;
-        x2:=x2/1.1;
-        x3:=x3/1.1;
         if iter > 1 then
+        begin
             iter:=iter-1;
+            dx2:=dx2/1.4;
+            dx3:=dx3/1.4;
+            dy2:=dy2/1.4;
+            dy3:=dy3/1.4;
+        end;
     end;
     'x':
     begin
-        y1:=y1*1.1;
-        y2:=y2*1.1;
-        y3:=y3*1.1;
-        x1:=x1*1.1;
-        x2:=x2*1.1;
-        x3:=x3*1.1;
         if iter < 7 then
+        begin
             iter:=iter+1;
+            dx2:=dx2*1.4;
+            dx3:=dx3*1.4;
+            dy2:=dy2*1.4;
+            dy3:=dy3*1.4;
+        end;
     end;
     End;
 end;
 
 begin
-    gd := Detect;
-    InitGraph(gd, gm, '');
-    iter:=4;
-    VisualPage := 1;
+    gd := detect;
+    initgraph(gd, gm, '');
+    iter:=3;
+    kp:=true;
+    visualpage := 1;
     x1:=320;y1:=20;
-    x2:=700;y2:=470;
-    x3:=40;y3:=470;
-    tr(x1,y1,x2,y2,x3,y3);
-    draw(x1,y1,x2,y2,x3,y3,iter);
-    while true do
+    dx2:=-200;dx3:=200;
+    dy2:=300;dy3:=300;
+    while kp do
         key_pres();
-    CloseGraph;
 end.
